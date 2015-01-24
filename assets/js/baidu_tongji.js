5892bcd431de1fef44d23fb552f14b6352ead899
cherrylee <cherryleer@gmail.com>
2015-01-24 17:01:12 +0800

    divide share.html

---
+++ assets/js/baidu_tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?43b99f06cd33b0ded620f4f4e5283574";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();

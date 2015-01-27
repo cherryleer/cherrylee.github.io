398b6643e5972a5c43a327996e388cca15bd2c8f
cherrylee <cherryleer@gmail.com>
2015-01-27 11:19:31 +0800

    change baidu tongji to cherryleer.com

--- assets/js/baidu_tongji.js
+++ assets/js/baidu_tongji.js
@@ -1,7 +1,9 @@
+<script>
 var _hmt = _hmt || [];
 (function() {
     var hm = document.createElement("script");
-    hm.src = "//hm.baidu.com/hm.js?43b99f06cd33b0ded620f4f4e5283574";
+    hm.src = "//hm.baidu.com/hm.js?faa73d4383dbfc682f8ccd0ae58971e4";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
-})();
+    })();
+</script>

0f682bd742a00866286e39909a77c8dbdbcec671
cherryleer <cherryleer@gmail.com>
2015-06-19 17:12:40 +0800

    add search function

--- assets/js/script.js
+++ assets/js/script.js
@@ -126,3 +126,29 @@ function afterPjax() {
 
 }afterPjax();
 
+// add by cherryleer @2015-6-19 for searching function
+$(function() {
+    $.ajax({
+        url: "/search.xml",
+        dataType: "xml",
+        success: function( xmlResponse ) {
+            var data = $( "article", xmlResponse ).map(function() {
+                return {
+                    value: $( "title", this ).text() + ", " +
+                    ( $.trim( $( "date", this ).text() ) ),
+                    desc: $("description", this).text(),
+                    url: $("url", this).text()
+                };
+            }).get();
+
+            $( "#search-input" ).autocomplete({
+                source: data,
+                minLength: 0,
+                select: function( event, ui ) {
+                    window.location.href = ui.item.url;
+                }
+            });
+        }
+    });
+});
+
